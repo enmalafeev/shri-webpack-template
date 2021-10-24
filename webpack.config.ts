ace95f367d956f65c1fc81d52598aba6b1361ad9
enmalafeev <malafeev.en@yandex.ru>
2021-10-24 22:42:09 +0300

    fix webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,20 +4,13 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import StatoscopePlugin from '@statoscope/webpack-plugin';
 
 // import ModuleLogger from './plugins/moduleLogger';
-import UnusedFilesPlugin from './plugins/UnusedFilesPlugin';
+// import UnusedFilesPlugin from './plugins/UnusedFilesPlugin';
 
 const config: webpack.Configuration = {
     mode: 'production',
     entry: {
-        root: {
-            import: './src/pages/root.tsx',
-            dependOn: 'shared',
-        },
-        root2: {
-            import: './src/pages/root2.tsx',
-            dependOn: 'shared',
-        },
-        shared: ['readable-stream', 'safe-buffer', 'bn.js'],
+        root: './src/pages/root.tsx',
+        root2: './src/pages/root2.tsx',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -32,7 +25,8 @@ const config: webpack.Configuration = {
     },
     plugins: [
         new HtmlWebpackPlugin(),
-        new UnusedFilesPlugin({ options: true }),
+        // new ModuleLogger(),
+        // new UnusedFilesPlugin({ options: true }),
         new StatoscopePlugin({
             saveStatsTo: 'stats.json',
             saveOnlyStats: false,
@@ -40,25 +34,16 @@ const config: webpack.Configuration = {
         }),
     ],
     resolve: {
-        alias: {
-            [path.resolve(__dirname, 'node_modules/asn1.js/node_modules/bn.js')]: false,
-            [path.resolve(__dirname, 'node_modules/elliptic/node_modules/bn.js')]: false,
-            [path.resolve(__dirname, 'node_modules/create-ecdh/node_modules/bn.js')]: false,
-            [path.resolve(__dirname, 'node_modules/miller-rabin/node_modules/bn.js')]: false,
-            [path.resolve(__dirname, 'node_modules/diffie-hellman/node_modules/bn.js')]: false,
-            [path.resolve(__dirname, 'node_modules/public-encrypt/node_modules/bn.js')]: false,
-
-            [path.resolve(__dirname, 'node_modules/bn.js')]: false,
-            [path.resolve(__dirname, 'node_modules/hash-base/node_modules/readable-stream')]: false,
-            [path.resolve(__dirname, 'node_modules/browserify-sign/node_modules/readable-stream')]: false,
-            [path.resolve(__dirname, 'node_modules/string_decoder/node_modules/safe-buffer')]: false,
-            [path.resolve(__dirname, 'node_modules/readable-stream/node_modules/safe-buffer')]: false,
-        },
         fallback: {
             'buffer': require.resolve('buffer'),
             'stream': false,
         },
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+            'bn.js': false,
+            'browserify-crypto': false,
+            'crypto-browserify': false,
+        },
     },
     module: {
         rules: [
